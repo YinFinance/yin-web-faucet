46eabc4d95a7ef5f4285855cd81eee2191f6d74b
YIN Maker <yinyangfinance@gmail.com>
2021-07-30 08:22:19 +0800

    add faucet link

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
     </header>
      <div className="case">
        <form className="form" onSubmit={mintTokens}>
+           <a className="link" href='https://faucet.rinkeby.io/' target="_blank">Get testnet ETH ↗</a>
           <p>Request tokens from YIN Finance Alpha Faucet</p>
           <p>You will get 200 <a href="https://rinkeby.etherscan.io/address/0x67751356e2ac7ca20cb991bf3edf7d4a439b7cbe" target='_blank'>tusdt</a>, 200<a href="https://rinkeby.etherscan.io/address/0x8543a038bef4de734b4bc2235a97f7eaf439625f" target='_blank'>tdai</a>, 200<a href="https://rinkeby.etherscan.io/address/0x130785fd1f629956d221d7226f4c03e1b692064a" target='_blank'>tfei</a>, 200<a href="https://rinkeby.etherscan.io/address/0x849cdf076a4cccc724258d88caebfd566e808305" target='_blank'>trai</a>, 200<a href="https://rinkeby.etherscan.io/address/0x3ad6b1f58e37f17f295edc74200ce140bd3020c3" target='_blank'>tusdc</a> in Rinkeby.</p>
 
